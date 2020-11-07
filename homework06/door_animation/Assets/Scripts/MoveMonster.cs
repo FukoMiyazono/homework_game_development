28c6e09b0b5d46f78e40034f00229e2afb70f3a1
FukoMiyazono <linyuxinxuan_14@126.com>
2020-11-07 12:09:03 +0800

    update homework06

---
+++ homework06/door_animation/Assets/Scripts/MoveMonster.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMonster : MonoBehaviour
+{
+    public Transform kPlayer;
+    public float fDistance = 5f;
+    private Vector3 deltaPos = Vector3.zero;
+
+    private Animator animator;
+    public GameObject sword;
+    public GameObject stone;
+    public Transform camera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float distance = Mathf.Sqrt(h * h + v * v);
+        float speed = distance / Time.deltaTime;
+        animator.SetFloat("MoveSpeed", speed);
+
+        bool fire = Input.GetButtonDown("Fire1");
+        if (fire)
+        {
+            animator.SetTrigger("Die");
+        }
+
+        
+    }
+
+
+    private void FixedUpdate()
+    {
+        Vector3 kNewPos = Camera.main.transform.position + fDistance * Camera.main.transform.forward;
+        kNewPos.y = 0;
+
+        kPlayer.position = kNewPos;
+       
+    }
+
+    public void Causefire()
+    {
+        Vector3 player_pos=sword.transform.position;
+        Vector3 stone_pos=stone.transform.position;
+        if (player_pos[0] < stone_pos[0] + 1.7 && player_pos[0] > stone_pos[0] - 1.7)
+        {
+            if (player_pos[2] < stone_pos[2] + 1.7 && player_pos[2] > stone_pos[2] - 1.7)
+            {
+                sword.GetComponent<ParticleSystem>().Play();
+                ScreenShake.isshakeCamera = true;
+            }
+        }
+        
+    }
+}
